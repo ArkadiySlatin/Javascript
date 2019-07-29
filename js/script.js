f82e034c357c6d239e6fe380fb33d6417e53aed8
Arkadiy <slatinarkadiy@yandex.ru>
2019-07-29 16:25:51 +0500

    Второе практическое задание

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц", "");
+let money = +prompt("Ваш бюджет на месяц", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 
@@ -12,15 +12,52 @@ let appData = {
     savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+for ( let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+    if ((typeof(a)) === 'string' && (typeof(a)) != null  && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 20 && b.length < 20) {
+        appData.expenses[a] = b;
+        
+    } else {
+        alert("Не правильно введены данные");
+        i--;
+    }
+}
+/* Второй вариент цикла */
+/* let i = 0;
+while (i < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ((typeof(a)) === 'string' && (typeof(a)) != null  && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 20 && b.length < 20) {
+        i++;
+        appData.expenses[a] = b;
+        
+        } else {
+            alert("Не правильно введены данные");
+            
+        }
+} */
+/* Третий вариант цикла */
+/* let i = 0;
+do {let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ((typeof(a)) === 'string' && (typeof(a)) != null  && (typeof(b)) != null
+    && a != '' && b != '' && a.length < 20 && b.length < 20) {
+    i++;
+    appData.expenses[a] = b;
+    
+    } else {
+        alert("Не правильно введены данные");
+    }
+} while (i < 2); */
 
-alert(appData.budget / 30);
+appData.moneyPerDay = appData.budget /30;
+alert("Бюджет на каждый день составляет: " + appData.moneyPerDay + " руб.");
+
+
 
 
 
